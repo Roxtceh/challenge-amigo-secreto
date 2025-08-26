79fac0c14cde40cffa08c1d6b328778ec468594b
Roxtceh <roxtceh13z@gmail.com>
2025-08-25 23:35:57 -0600

    Crea función agregarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 let nombres=[];
+
+function agregarAmigo(){
+    let elemento=document.getElementById(`amigo`);
+    let nombre=elemento.value;
+    if(nombre==""){
+        alert("Por favor inserte un nombre");
+    }else{
+        nombres.push(nombre);
+        elemento.value="";
+    }
+}
