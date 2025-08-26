5ddf9eb7dea437aee86d30569407db091fa41db6
Roxtceh <roxtceh13z@gmail.com>
2025-08-26 14:08:52 -0600

    Crea función sortearAmigo()

--- app.js
+++ app.js
@@ -22,3 +22,20 @@ function actualizarLista(nombres){
         lista.appendChild(nuevoLi);
     }
 }
+
+function sortearAmigo(){
+    let ganador="";
+    let numeroSorteado;
+    let lista=document.getElementById(`listaAmigos`);
+    let listaGanador=document.getElementById(`resultado`);
+    let nuevoLi=document.createElement(`li`);
+    if (nombres.length==0) {
+        alert(`Introduce al menos un nombre en la lista`);
+    } else {
+        numeroSorteado=Math.floor(Math.random()*nombres.length);
+        ganador=nombres[numeroSorteado];
+        nuevoLi.textContent="El amigo secreto sorteado es: "+nombres[numeroSorteado];
+        listaGanador.appendChild(nuevoLi);
+        lista.innerHTML="";
+    }
+}
